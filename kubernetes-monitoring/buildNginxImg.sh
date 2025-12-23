b848c89a1f2244b42f5d6d08984c4b3ec6f7efa3
sanders2022 <shnaideralex@gmail.com>
2025-12-23 22:38:05 +0300

    almost done

--- kubernetes-monitoring/buildNginxImg.sh
+++ kubernetes-monitoring/buildNginxImg.sh
@@ -1,2 +1,19 @@
-nginxversion=1.29.4
-docker buildx build --build-arg "NGINX_VERSION=$nginxeversion" -t nginxstub:${nginxversion} .
+set -e
+
+nginxversion=1.27.4
+USERNAME='Sanders-74'
+IMAGE_NAME="ghcr.io/${USERNAME,,}/nginxstub:${nginxversion}"
+
+# Check if the token variable is set
+if [ -z "$GITHUBTOKEN" ]; then
+    echo "Error: GITHUBTOKEN environment variable is not set."
+    exit 1
+fi
+docker buildx build --load --build-arg "NGINX_VERSION=$nginxversion" -t nginxstub:${nginxversion} .
+docker login ghcr.io -u ${USERNAME} -p=${GITHUBTOKEN}
+if [ $? -ne 0 ]; then
+    echo "Error: Docker login failed. Check your token and permissions."
+    exit 1
+fi
+docker tag nginxstub:${nginxversion} ${IMAGE_NAME}
+docker push ${IMAGE_NAME}
